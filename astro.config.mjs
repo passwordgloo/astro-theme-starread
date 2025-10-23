ce938885cb4a3c1b1a901208ae246a5fe8b9eda3
foresee <passwordgloo@163.com>
2025-10-23 19:53:21 +0800

    feat: 添加自动更新永久链接和索引功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import autoUpdatePermalink from './scripts/autoUpdateFrontmatter.js';
+import autoIndex from './scripts/autoindex.js';
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://star.iglooblog.top",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), autoUpdatePermalink(), autoIndex()],
 });
